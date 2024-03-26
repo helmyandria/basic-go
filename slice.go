19b1a68498e7983a0263ec63948ca6af7e19853b
Helmy Andrianto <helmyandria@gmail.com>
2024-03-26 08:41:22 +0700

    slice, array

---
+++ slice.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+func main() {
+	text := [...]string{"Go", "Python", "Java", "Ruby", "C#", "C++", "Rust"}
+
+	slice1 := text[4:7]
+	fmt.Println(slice1)
+
+	slice2 := text[:3]
+	fmt.Println(slice2)
+
+	slice3 := text[3:]
+	fmt.Println(slice3)
+
+	slice4 := text[:]
+	fmt.Println(slice4)
+
+	days := [...]string{"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"}
+	daySlice1 := days[5:]
+	daySlice1[0] = "Sabtu Baru"
+	daySlice1[1] = "Minggu Baru"
+	fmt.Println(days)
+
+	daySlice2 := append(daySlice1, "Libur Baru")
+	daySlice2[0] = "Ups"
+	fmt.Println(daySlice2)
+	fmt.Println(days)
+
+	var newSlice []string = make([]string, 2, 5)
+	newSlice[0] = "Belajar"
+	newSlice[1] = "Go"
+	// newSlice[2] = "Lang" //error harusnya menggunakan append
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+
+	newSlice2 := append(newSlice, "Lang")
+	fmt.Println(newSlice2)
+	fmt.Println(len(newSlice2))
+	fmt.Println(cap(newSlice2))
+
+	newSlice2[0] = "Sinau"
+	fmt.Println(newSlice2)
+	fmt.Println(newSlice)
+
+	fromSlice := days[:]
+	toSlice := make([]string, len(fromSlice), cap(fromSlice))
+
+	copy(toSlice, fromSlice)
+	fmt.Println(fromSlice)
+	fmt.Println(toSlice)
+
+	iniArray := [...]int{1, 2, 3, 4, 5}
+	iniSlice := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(iniArray)
+	fmt.Println(iniSlice)
+
+}
