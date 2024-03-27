ffcf5f4e44c7c8a6ffa1981a0afee5b9d2bed42a
Helmy Andrianto <helmyandria@gmail.com>
2024-03-27 13:54:28 +0700

    function return value

---
+++ function_return_value.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func getHello(name string) string {
+	hello := "Hello " + name
+
+	return hello
+}
+
+func main() {
+	result := getHello("GO")
+
+	fmt.Println(result)
+	fmt.Println(getHello("Lang"))
+}
