f25603879def7078884b8c2da9ae77fa2b74b242
Helmy Andrianto <helmyandria@gmail.com>
2024-04-02 14:34:53 +0700

    access modifier

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,12 @@
 package helper
 
+var version = "1.0.0"      //huruf awal kecil tidak bisa diakses dari luar class
+var Application = "golang" //huruf awal besar bisa diakses dari luar class
+
+func sayGoodBye(name string) string {
+	return "Good Bye " + name
+}
+
 func SayHello(name string) string {
 	return "Hello " + name
 }
--- import.go
+++ import.go
@@ -8,4 +8,7 @@ import (
 func main() {
 	result := helper.SayHello("Go")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application) //berhasil diakses di luar class
+	// fmt.Println(helper.version) //gagal diakses di luar class
 }
